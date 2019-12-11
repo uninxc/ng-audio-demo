b199168d68b4b64b3916dd514035edc234dbc118
兴百超 <xingbaichao@ehaier.com>
2019-12-11 18:13:10 +0800

    hlsjs

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 ### Installation
 
     npm i plyr ngx-plyr
+    npm i -s @angular/flex-layout @angular/cdk
+    npm install --save @angular/material @angular/animations @angular/cdk
+    
     
 ### Features
 * Stream
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { PlyrModule } from 'ngx-plyr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
+import {HlsjsComponent} from './hlsjs/hlsjs.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerComponent,
+    HlsjsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PlyrModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
